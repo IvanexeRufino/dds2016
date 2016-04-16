e0c29ec77247b7a2c5bae742edb7f02f35572572
sora797 <natitorokian@gmail.com>
2016-04-16 19:14:09 -0300

    agrego clase coordenada

---
+++ src/main/java/com/ddsutn/group01/tpanual/models/pois/Coordenada.java
@@ -0,0 +1,20 @@
+package com.ddsutn.group01.tpanual.models.pois;
+
+public class Coordenada {
+
+	private int x;
+	private int y;
+	
+	public Coordenada (int valor1, int valor2) {
+		this.x = valor1;
+		this.y = valor2;
+	}
+	
+	public int getX () {
+		return x;
+	}
+	
+	public int getY () {
+		return y;
+	}
+}
