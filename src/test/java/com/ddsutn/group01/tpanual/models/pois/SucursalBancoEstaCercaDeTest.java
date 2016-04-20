b138fb3a49997a5181bf8fface6f76f2caf19540
sora797 <natitorokian@gmail.com>
2016-04-20 00:12:38 -0300

    test de cercania de la clase SucursalBanco

---
+++ src/test/java/com/ddsutn/group01/tpanual/models/pois/SucursalBancoEstaCercaDeTest.java
@@ -0,0 +1,32 @@
+package com.ddsutn.group01.tpanual.models.pois;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.uqbar.geodds.Point;
+
+public class SucursalBancoEstaCercaDeTest {
+
+    private Point point;
+    private SucursalBanco santander;
+    private Point anotherPoint;
+    private Point anotherAnotherPoint;
+
+    @Before
+    public void init() {
+    	point = new Point(-34.603689, -58.381652); // https://goo.gl/maps/NquccBrGJsz
+    	anotherPoint = new Point(-34.601921, -58.381701); // https://goo.gl/maps/P9bmo5D2P8r
+    	anotherAnotherPoint = new Point (-34.6039217, -58.399196);
+        santander = new SucursalBanco("santander", point);
+    }
+
+    @Test
+    public void BancoEstaCercaDe() {
+        Assert.assertTrue(santander.estaCercaDe(anotherPoint));
+    }
+
+    @Test
+    public void BancoNoEstaCercaDe() {
+    	 Assert.assertFalse(santander.estaCercaDe(anotherAnotherPoint));
+    }
+}
