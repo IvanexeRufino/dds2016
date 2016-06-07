6e8d9697be0183898bb1058cb03e6f8f8385bf53
sora797 <natitorokian@gmail.com>
2016-06-07 15:49:18 -0300

    tests CGPOrigin

---
+++ src/test/java/com/ddsutn/group01/tpanual/models/mocks/ExternalSourceCGPMock.java
@@ -0,0 +1,24 @@
+package com.ddsutn.group01.tpanual.models.mocks;
+
+import java.util.ArrayList;
+
+
+import com.ddsutn.group01.tpanual.adapters.CGP.CentroDTO;
+import com.ddsutn.group01.tpanual.adapters.CGP.ServicioDTO;
+import com.ddsutn.group01.tpanual.externalSources.ExternalSourceCGP;
+
+
+public class ExternalSourceCGPMock implements ExternalSourceCGP {
+	@Override
+	public ArrayList<CentroDTO> search(String criteria) {
+		ArrayList<ServicioDTO> servicios = new ArrayList<ServicioDTO>();
+		ArrayList<String> zonas = new ArrayList<String>();
+		zonas.add("caballito");			
+		CentroDTO unCentroDTO = new CentroDTO(2, zonas, "juancho perez", "rivadavia 4450", "45682315", servicios);
+		
+		ArrayList<CentroDTO> resultados = new ArrayList<CentroDTO>();
+		resultados.add(unCentroDTO);
+		
+		return resultados;		
+	}
+}
--- src/test/java/com/ddsutn/group01/tpanual/origins/CGPsOriginTest.java
+++ src/test/java/com/ddsutn/group01/tpanual/origins/CGPsOriginTest.java
@@ -1,9 +1,21 @@
 package com.ddsutn.group01.tpanual.origins;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import com.ddsutn.group01.tpanual.models.mocks.ExternalSourceCGPMock;
 
 public class CGPsOriginTest {
+    private CGPsOrigin origin;
+
+    @Before
+    public void setUp() throws Exception {
+        ExternalSourceCGPMock externalSource = new ExternalSourceCGPMock();
+        origin = new CGPsOrigin(externalSource);
+    }
+    
     @Test
     public void find() throws Exception {
+        Assert.assertFalse(origin.find("rivadavia").isEmpty());
     }
 }
