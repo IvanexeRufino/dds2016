d89d8fa0fe11d01f4b77e00649c99a8ebae2eb21
Matías Caricato <mattiascaricato@gmail.com>
2016-06-08 21:08:21 -0300

    Improve code style

--- src/main/java/com/ddsutn/group01/tpanual/tools/SearchReporter.java
+++ src/main/java/com/ddsutn/group01/tpanual/tools/SearchReporter.java
@@ -1,26 +1,26 @@
 package com.ddsutn.group01.tpanual.tools;
 
-import java.util.HashMap;
-
 import org.joda.time.LocalDate;
+
+import java.util.HashMap;
 
 public class SearchReporter implements Reporter {
 
-    private HashMap<LocalDate,Integer> reporte = new HashMap<>();
+    private HashMap<LocalDate, Integer> data = new HashMap<>();
 
     public void updateReport() {
         LocalDate date = LocalDate.now();
-        if (reporte.containsKey(date))
-        {   Integer CantidadDeResultados = reporte.get(date);
-            CantidadDeResultados += CantidadDeResultados;
-            reporte.put(date, CantidadDeResultados);
-        }
-        else {
-            reporte.put(date, 1);
+        if (data.containsKey(date)) {
+            Integer resultsCounter = data.get(date);
+            resultsCounter++;
+
+            data.put(date, resultsCounter);
+        } else {
+            data.put(date, 1);
         }
     }
 
-    public HashMap<LocalDate,Integer> obtenerReporte() {
-        return reporte;
+    public HashMap<LocalDate, Integer> getReport() {
+        return data;
     }
 }
