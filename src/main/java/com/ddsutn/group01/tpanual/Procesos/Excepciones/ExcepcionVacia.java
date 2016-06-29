0826e3271e1b9c863554ccf380ccd05db0247191
ivanexerufino <ivanexerufino@gmail.com>
2016-06-28 23:53:40 -0300

    Excepcion Vacia

---
+++ src/main/java/com/ddsutn/group01/tpanual/Procesos/Excepciones/ExcepcionVacia.java
@@ -0,0 +1,11 @@
+package com.ddsutn.group01.tpanual.Procesos.Excepciones;
+
+import com.ddsutn.group01.tpanual.Procesos.Proceso;
+
+public class ExcepcionVacia implements ExcepcionProceso {
+
+    @Override
+    public void manejarError(Proceso process, Exception error) throws Exception {      
+    }
+
+}
