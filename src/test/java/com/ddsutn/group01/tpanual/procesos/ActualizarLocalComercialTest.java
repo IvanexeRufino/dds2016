9bbaadc3836ce3b1ff4e6d1b1d28b896678f1253
sora797 <natitorokian@gmail.com>
2016-06-29 17:16:47 -0300

    el test, sigo ajustando el metodo ejecutar que no funca

--- src/test/java/com/ddsutn/group01/tpanual/procesos/ActualizarLocalComercialTest.java
+++ src/test/java/com/ddsutn/group01/tpanual/procesos/ActualizarLocalComercialTest.java
@@ -18,28 +18,40 @@ import com.ddsutn.group01.tpanual.repositories.PoiRepository;
 
 public class ActualizarLocalComercialTest {
 	private LocalComercial carrousel;
+	private LocalComercial kiosquito;
     private Point point;
     private HorariosDeAtencion horarios;
     private Rubro rubro;
 	
 	
 	@Before
-	public void init() {
-	PoiRepository repo = PoiRepository.getInstance();
-	
+	public void init() {	
 	point = new Point(1,1);
     horarios = new HorariosDeAtencion();
     rubro = Rubro.kiosco;
 
     carrousel = new LocalComercial(1,"carrousel",point,rubro,horarios);
-	carrousel.agregarPalabraClave("hola");
-   
+	carrousel.agregarPalabraClave("colegio");
+	
+	kiosquito = new LocalComercial (2,"kiosquito",point,rubro,horarios);
+	kiosquito.agregarPalabraClave("chupelupe");
+	
+	PoiRepository.getInstance().add(carrousel);
+	PoiRepository.getInstance().add(kiosquito);
 	}
 
 	@Test
-	public void testActualizarPalabrasClaves() {		
-		//carrousel.actualizarPalabrasClaves("juguete pelota");
-		Assert.assertEquals(carrousel.getPalabrasClaves().size(), 1);
+	public void testActualizarPalabrasClaves() {
+		carrousel.actualizarPalabrasClaves("chocolate caramelos chupelupe");
+		Assert.assertEquals(carrousel.getPalabrasClaves().size(), 3);
+		Assert.assertTrue(carrousel.getPalabrasClaves().contains("chocolate"));
+		Assert.assertTrue(carrousel.getPalabrasClaves().contains("caramelos"));
+		Assert.assertTrue(carrousel.getPalabrasClaves().contains("chupelupe"));
+	}
+	
+	@Test
+	public void testEjecutarActualizacionLocalComercial() {
+		
 	}
 
 }
