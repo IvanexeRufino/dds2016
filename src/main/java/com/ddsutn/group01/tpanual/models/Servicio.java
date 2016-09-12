cefab5ace2746ed05650b40ef1a9100e95cf88e5
Matías Caricato <mattiascaricato@gmail.com>
2016-09-12 12:31:19 -0300

    Add persistence config

--- src/main/java/com/ddsutn/group01/tpanual/models/Servicio.java
+++ src/main/java/com/ddsutn/group01/tpanual/models/Servicio.java
@@ -2,9 +2,17 @@ package com.ddsutn.group01.tpanual.models;
 
 import org.joda.time.DateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Servicio {
     private String nombre;
     private HorariosDeAtencion horariosDeAtencion;
+
+    @Id @GeneratedValue
+    private int id;
 
     public Servicio(String nombre, HorariosDeAtencion horariosDeAtencion) {
         this.nombre = nombre;
@@ -14,7 +22,7 @@ public class Servicio {
     public String getNombre() {
         return nombre;
     }
-    
+
     public HorariosDeAtencion getHorariosDeAtencion() {
         return horariosDeAtencion;
     }
