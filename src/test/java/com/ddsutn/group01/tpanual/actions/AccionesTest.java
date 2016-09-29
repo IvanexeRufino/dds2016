f900a5a83168d914b3f5e6e49e883974f9897cde
Matías Caricato <mattiascaricato@gmail.com>
2016-09-29 00:45:04 -0300

    Fix test

--- src/test/java/com/ddsutn/group01/tpanual/actions/AccionesTest.java
+++ src/test/java/com/ddsutn/group01/tpanual/actions/AccionesTest.java
@@ -1,11 +1,19 @@
 package com.ddsutn.group01.tpanual.actions;
 
+import com.ddsutn.group01.tpanual.buscador.ResultadoBusqueda;
+import com.ddsutn.group01.tpanual.db.BigDecimalConverter;
+import com.ddsutn.group01.tpanual.db.JodaDateTimeConverter;
+import com.ddsutn.group01.tpanual.db.JodaLocalTimeConverter;
 import com.ddsutn.group01.tpanual.models.pois.PointOfInterest;
 import com.ddsutn.group01.tpanual.buscador.Buscador;
 import com.ddsutn.group01.tpanual.roles.Terminal;
+import com.mongodb.MongoClient;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
@@ -17,6 +25,7 @@ public class AccionesTest extends AbstractPersistenceTest implements WithGlobalE
     private Action mockedAction;
     private Terminal unaTerminal;
     private Buscador buscador = new Buscador();
+    private Datastore datastore;
 
     @Before
     public void setUp() throws Exception {
@@ -24,6 +33,20 @@ public class AccionesTest extends AbstractPersistenceTest implements WithGlobalE
         mockedAction = Mockito.mock(Action.class);
 
         unaTerminal.addAction(mockedAction);
+
+        final Morphia morphia = new Morphia();
+        morphia.getMapper().getConverters().addConverter(BigDecimalConverter.class);
+        morphia.getMapper().getConverters().addConverter(JodaDateTimeConverter.class);
+        morphia.getMapper().getConverters().addConverter(JodaLocalTimeConverter.class);
+        morphia.mapPackage("com.ddsutn.group01.tpanual.buscador");
+        morphia.mapPackage("com.ddsutn.group01.tpanual.models.pois");
+        datastore = morphia.createDatastore(new MongoClient(), "pois");
+        datastore.ensureIndexes();
+    }
+
+    @After
+    public void terminate() {
+        datastore.delete(datastore.createQuery(ResultadoBusqueda.class));
     }
 
     @Test
