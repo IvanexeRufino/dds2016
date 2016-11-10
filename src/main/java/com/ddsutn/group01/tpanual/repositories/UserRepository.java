6a9dcce6b09aee11fd3a16729f20eb095f8d4ad3
IvanPC <IvanPC@Ivan>
2016-11-10 01:44:55 -0300

    100% work login

--- src/main/java/com/ddsutn/group01/tpanual/repositories/UserRepository.java
+++ src/main/java/com/ddsutn/group01/tpanual/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.ddsutn.group01.tpanual.repositories;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
@@ -9,13 +10,14 @@ import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 import com.ddsutn.group01.tpanual.roles.User;
 
 public class UserRepository implements WithGlobalEntityManager, EntityManagerOps, TransactionalOps{
-    public User authenticate(String userName, String password) {
+    public User authenticate(String username, String password) {
     	
     	List<User> usuarios = (List<User>)createQuery("FROM User").getResultList();
-    	usuarios.stream().filter(terminal->terminal.getUsername().equals("user") && terminal.getPassword().equals("password"));
+    	List<User> filtros = usuarios.stream().filter(terminal->terminal.getUsername().equals(username) && terminal.getPassword().equals(password))
+    									      .collect(Collectors.toList());
 
-        if (usuarios.size() == 1) {
-            return usuarios.get(0);
+        if (filtros.size() == 1) {
+            return filtros.get(0);
         }
 
         return null;
